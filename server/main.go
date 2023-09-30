88e640dcdda49e9a753126a0e77fe1cdbc2828f9
PO Boisvert <pob944@hotmail.com>
2023-09-30 13:59:22 -0400

    get all items

--- server/main.go
+++ server/main.go
@@ -6,13 +6,6 @@ import (
 	"github.com/poboisvert/GO_Fiber/server/server"
 )
 
-type Todo struct {
-	ID    int  `json:"id"`
-	Title int  `json:"title"`
-	Done  bool `json:"done"`
-	Body  bool `json:"body"`
-}
-
 func main() {
 	fmt.Print("Hello World")
 	server.InitializeApp()
--- server/server/server.go
+++ server/server/server.go
@@ -4,16 +4,67 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
 
 func InitializeApp() {
 
-	router := fiber.New()
+	app := fiber.New()
 
-	router.Get("/healthcheck", func(c *fiber.Ctx) error {
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
+	todos := []Todo{}
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
-	log.Fatal(router.Listen(":4000"))
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid ID")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
 
 }
